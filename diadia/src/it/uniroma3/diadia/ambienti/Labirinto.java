8ddee885b72f2fc40cec28d7c156024e278e7ffe
FedericoMinelli <fedemine3@gmail.com>
2025-03-27 00:41:05 +0100

    Riorganizzazione stanze del labirinto

--- diadia/src/it/uniroma3/diadia/ambienti/Labirinto.java
+++ diadia/src/it/uniroma3/diadia/ambienti/Labirinto.java
@@ -26,18 +26,29 @@ public class Labirinto {
 		Stanza biblioteca = new Stanza("Biblioteca");
 		
 		/* collega le stanze */
+		
 		atrio.impostaStanzaAdiacente("nord", biblioteca);
 		atrio.impostaStanzaAdiacente("est", aulaN11);
 		atrio.impostaStanzaAdiacente("sud", aulaN10);
 		atrio.impostaStanzaAdiacente("ovest", laboratorio);
-		aulaN11.impostaStanzaAdiacente("est", laboratorio);
 		aulaN11.impostaStanzaAdiacente("ovest", atrio);
 		aulaN10.impostaStanzaAdiacente("nord", atrio);
-		aulaN10.impostaStanzaAdiacente("est", aulaN11);
-		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
 		laboratorio.impostaStanzaAdiacente("est", atrio);
-		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
 		biblioteca.impostaStanzaAdiacente("sud", atrio);
+		
+		/* vecchio collegamento, da lasciare in caso di cambi futuri */
+//		atrio.impostaStanzaAdiacente("nord", biblioteca);
+//		atrio.impostaStanzaAdiacente("est", aulaN11);
+//		atrio.impostaStanzaAdiacente("sud", aulaN10);
+//		atrio.impostaStanzaAdiacente("ovest", laboratorio);
+//		aulaN11.impostaStanzaAdiacente("est", laboratorio);		// effetto pac-man
+//		aulaN11.impostaStanzaAdiacente("ovest", atrio);
+//		aulaN10.impostaStanzaAdiacente("nord", atrio);
+//		aulaN10.impostaStanzaAdiacente("est", aulaN11);			// monodirezionale n10 -> n11
+//		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);	// monodirezionale n10 -> lab
+//		laboratorio.impostaStanzaAdiacente("est", atrio);
+//		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);	// effetto pac-man
+//		biblioteca.impostaStanzaAdiacente("sud", atrio);
 
         /* pone gli attrezzi nelle stanze */
 		aulaN10.addAttrezzo(lanterna);
