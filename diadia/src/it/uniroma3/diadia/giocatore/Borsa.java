70dee91e92274000158daab76c1c3022aa933f2c
FedericoMinelli <fedemine3@gmail.com>
2025-03-27 23:12:08 +0100

    Commentati i principali metodi di Borsa

--- diadia/src/it/uniroma3/diadia/giocatore/Borsa.java
+++ diadia/src/it/uniroma3/diadia/giocatore/Borsa.java
@@ -5,30 +5,56 @@ import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 
 public class Borsa {
+	
 	public final static int DEFAULT_PESO_MAX_BORSA = 10;
 	private Attrezzo[] attrezzi;
 	private int numeroAttrezzi;
 	private int pesoMax;
+	
+	/**
+	 * Costruttori per classe Borsa
+	 */
 	public Borsa() {
 		this(DEFAULT_PESO_MAX_BORSA);
 	}
+	
 	public Borsa(int pesoMax) {
 		this.pesoMax = pesoMax;
 		this.attrezzi = new Attrezzo[10]; 
 		this.numeroAttrezzi = 0;
 	}
+	
+	/**
+	 * Prova ad aggiungere 'attrezzo' alla borsa
+	 * @param attrezzo
+	 * @return false: quando aggiunta dell'attrezzo fa andare il peso totale sopra il valore max o la borsa è piena, true: altrimenti
+	 */
 	public boolean addAttrezzo(Attrezzo attrezzo) {
 		if (this.getPeso() + attrezzo.getPeso() > this.getPesoMax())
 			return false;
+		
 		if (this.numeroAttrezzi==10)
 			return false;
+		
 		this.attrezzi[this.numeroAttrezzi] = attrezzo;
 		this.numeroAttrezzi++;
+		
 		return true;
 	}
+	
+	/**
+	 * Getter di pesoMax
+	 * @return pesoMax
+	 */
 	public int getPesoMax() {
 		return pesoMax;
 	}
+	
+	/**
+	 * Metodo che restituisce l'attrezzo di nome 'nomeAttrezzo' se presente
+	 * @param nomeAttrezzo
+	 * @return attrezzo di nome passato come parametro formale, null se non viene trovato
+	 */
 	public Attrezzo getAttrezzo(String nomeAttrezzo) {
 		Attrezzo a = null;
 		for (int i= 0; i<this.numeroAttrezzi; i++)
@@ -38,20 +64,35 @@ public class Borsa {
 		return a;
 	}
 
-
+	/**
+	 * Metodo che somma il peso di tutti gli attrezzi presenti nella borsa
+	 * @return peso attuale della borsa
+	 */
 	public int getPeso() {
 		int peso = 0;
-		for (int i= 0; i<this.numeroAttrezzi; i++)
+		for (int i=0; i<this.numeroAttrezzi; i++)
 			peso += this.attrezzi[i].getPeso();
 
 		return peso;
 	}
+	
+	/**
+	 * Verifica se la borsa è vuota
+	 * @return	true se la borsa è vuota, false altrimenti
+	 */
 	public boolean isEmpty() {
 		return this.numeroAttrezzi == 0;
 	}
+	
+	/**
+	 * Verifica se 
+	 * @param nomeAttrezzo
+	 * @return
+	 */
 	public boolean hasAttrezzo(String nomeAttrezzo) {
 		return this.getAttrezzo(nomeAttrezzo)!=null;
 	}
+	
 	/**
 	 * Rimuove un attrezzo dalla borsa (ricerca in base al nome).
 	 * @param nomeAttrezzo
@@ -76,6 +117,7 @@ public class Borsa {
 
 		return a;
 	}
+	
 	public String toString() {
 		StringBuilder s = new StringBuilder();
 
