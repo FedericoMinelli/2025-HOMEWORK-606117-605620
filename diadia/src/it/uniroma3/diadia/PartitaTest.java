1b8af724157bedf0b3ba3f9d820b501be29b2298
FedericoMinelli <fedemine3@gmail.com>
2025-03-31 01:02:48 +0200

    Aggiunta classe LabirintoTest e sistemata classe PartitaTest

--- diadia/src/it/uniroma3/diadia/PartitaTest.java
+++ diadia/src/it/uniroma3/diadia/PartitaTest.java
@@ -5,38 +5,39 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import it.uniroma3.diadia.ambienti.Stanza;
-
 class PartitaTest {
 	private Partita partita;
-	private Stanza vincente;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		this.partita=new Partita();
-		this.vincente=new Stanza("biblioteca");
 	}
 
 	@Test
-	void testNuovaPartitaNonFinita() {
+	void testPartitaNonFinita() {
 		assertFalse(this.partita.isFinita());
 	}
+	
+	@Test
+	void testPartitaFinita_ZeroCFU() {
+		this.partita.getGiocatore().setCfu(0);
+		assertTrue(this.partita.isFinita());
+	}
+	
+	@Test
+	void testPartitaFinita_SettataAFinita() {
+		this.partita.setFinita();
+		assertTrue(this.partita.isFinita());
+	}
+	
 	@Test
 	void testPartitaVinta() {
 		partita.setStanzaCorrente(partita.getStanzaVincente());
 		assertTrue(this.partita.vinta());
 	}
-	//@Test
-	void testPartitaPersa() {
-		partita.getGiocatore().setCfu(0);
+	
+	@Test
+	void testPartitaNonAncoraVinta() {
 		assertFalse(this.partita.vinta());
 	}
-	@Test
-	void testPartitaNonFinitaEPoiFinita() {
-		assertFalse(this.partita.isFinita());
-		this.partita.setFinita();
-		assertTrue(this.partita.isFinita());
-	}
-	
-
 }
