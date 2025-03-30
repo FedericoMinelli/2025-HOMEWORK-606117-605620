793be7244576be2fb870257f4b01e092a264fb2f
Nicolò <nic.tomei3@stud.uniroma3.it>
2025-03-30 11:53:02 +0200

    ampliati BorsaTest e StanzaTest

--- diadia/src/it/uniroma3/diadia/ambienti/StanzaTest.java
+++ diadia/src/it/uniroma3/diadia/ambienti/StanzaTest.java
@@ -11,29 +11,52 @@ class StanzaTest {
 
 	private Stanza stanza;
 	private Attrezzo spada;
+	private Attrezzo osso;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		this.stanza=new Stanza("n11");
 		this.spada = new Attrezzo("spada", 10);
+		this.osso = new Attrezzo("osso", 5);
 	}
 
 	@Test
-	void testHasAttrezzoStanzaVuota() {
+	void testHasAttrezzo_StanzaVuota() {
 		assertFalse(this.stanza.hasAttrezzo("spada"));
 	}
 	@Test
-	void testHasAttrezzo() {
+	void testHasAttrezzo_StanzaAttrezzoGiusto() {
 		this.stanza.addAttrezzo(this.spada);
 		assertTrue(this.stanza.hasAttrezzo("spada"));
 	}
+	
 	@Test
-	void testRemoveAttrezzo() {
+    void testHasAttrezzo_StanzaAttrezzoSbagliato() {
+		this.stanza.addAttrezzo(this.osso);
+        assertFalse(this.stanza.hasAttrezzo("spada"));
+    }
+	
+	// provo a rimuovere da una stanza che non ha quel attrezzo ed è vuota
+	@Test
+	void testRemoveAttrezzo_StanzaVuota() {
 		assertFalse(this.stanza.removeAttrezzo(spada));
-		
+	}
+	
+	// provo a rimuovere un attrezzo presente nella stanza
+	@Test
+	void testRemoveAttrezzo_StanzaOggettoPresente() {
 		this.stanza.addAttrezzo(this.spada);
 		assertTrue(this.stanza.removeAttrezzo(spada));
-		
 	}
+	
+	// provo a rimuovere da una stanza che non ha quell'attrezzo ma ne ha un altro
+	@Test
+	void testRemoveAttrezzo_StanzaOggettoNonPresente() {
+		this.stanza.addAttrezzo(this.osso);
+		assertFalse(this.stanza.removeAttrezzo(spada));
+	}
+	
+	
+	
 	
 }
--- diadia/src/it/uniroma3/diadia/giocatore/BorsaTest.java
+++ diadia/src/it/uniroma3/diadia/giocatore/BorsaTest.java
@@ -11,31 +11,80 @@ class BorsaTest {
 
 	private Borsa borsa;
 	private Attrezzo osso;
+	private Attrezzo spada;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		this.borsa= new Borsa(20);
 		this.osso= new Attrezzo("osso", 2);
+		this.spada= new Attrezzo("spada", 10);
 	}
 
+	
 	@Test
-	void testBorsaHasAttrezzo() {
+	void testBorsaHasAttrezzo_BorsaVuota() {
+		assertFalse(this.borsa.hasAttrezzo("osso"));
+	}
+	
+	@Test
+	void testBorsaHasAttrezzo_AttrezzoPresente() {
 		this.borsa.addAttrezzo(this.osso);
 		assertTrue(this.borsa.hasAttrezzo("osso"));
 	}
+	
+	@Test
+	void testBorsaHasAttrezzo_AttrezzoNonPresente() {
+		this.borsa.addAttrezzo(this.spada);
+		assertFalse(this.borsa.hasAttrezzo("osso"));
+	}
+	
 	@Test
 	void testBorsaVuota() {
 		assertTrue(this.borsa.isEmpty());
 	}
+	
 	@Test
-	// non uso assertTrue/False perchè RemoveAttrezzo non restituisce un booleano
-	void testBorsaRemoveAttrezzo() {
-		// se la borsa non contiene l'attrezzo restituisce null;
+	void testBorsaNonVuota() {
+		this.borsa.addAttrezzo(osso);
+		assertFalse(this.borsa.isEmpty());
+	}
+
+	@Test
+	// se la borsa non contiene l'attrezzo restituisce null
+	void testBorsaRemoveAttrezzo_BorsaVuota() {
 		assertNull(this.borsa.removeAttrezzo("osso"));
-		// aggiunge l'attrezzo e verifica se la rimozione avviene correttamente
+	}
+	
+	@Test
+	// aggiunge l'attrezzo e verifica se la rimozione avviene correttamente
+	void testBorsaRemoveAttrezzo_AttrezzoPresente() {
 		this.borsa.addAttrezzo(this.osso);
 		assertEquals(this.osso, this.borsa.removeAttrezzo("osso"));
+	}
+	
+	@Test
+	// prova ad eliminare un attrezzo non presente nella borsa, return null
+	void testBorsaRemoveAttrezzo_AttrezzoNonPresente() {
+		this.borsa.addAttrezzo(this.spada);
+		assertNull(this.borsa.removeAttrezzo("osso"));
+	}
+	
 
+	@Test
+	void testBorsaGetAttrezzo_BorsaVuota() {
+		assertNull(this.borsa.getAttrezzo("osso"));
+	}
+	
+	@Test
+	void testBorsaGetAttrezzo_OggettoPresente() {
+		this.borsa.addAttrezzo(osso);
+		assertEquals(this.osso, this.borsa.getAttrezzo("osso"));
+	}
+	
+	@Test
+	void testBorsaGetAttrezzo_OggettoNonPresente() {
+		this.borsa.addAttrezzo(spada);
+		assertNull(this.borsa.getAttrezzo("osso"));
 	}
 
 }
