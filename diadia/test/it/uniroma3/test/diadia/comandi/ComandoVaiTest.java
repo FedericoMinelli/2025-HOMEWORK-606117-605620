1c6f874e474edab37743a93fee5516c3a636e4d6
FedericoMinelli <fedemine3@gmail.com>
2025-05-30 12:59:47 +0200

    fixati dei test di comandoVai, da iniziare esercizio 8

--- diadia/test/it/uniroma3/test/diadia/comandi/ComandoVaiTest.java
+++ diadia/test/it/uniroma3/test/diadia/comandi/ComandoVaiTest.java
@@ -9,20 +9,34 @@ import it.uniroma3.diadia.IOConsole;
 import it.uniroma3.diadia.Partita;
 import it.uniroma3.diadia.ambienti.Stanza;
 import it.uniroma3.diadia.comandi.ComandoVai;
-//import it.uniroma3.diadia.ambienti.StanzaBloccata;
-//import it.uniroma3.diadia.attrezzi.Attrezzo;
-//import it.uniroma3.diadia.comandi.ComandoGuarda;
+import it.uniroma3.diadia.ambienti.StanzaBloccata;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.comandi.ComandoGuarda;
 
 class ComandoVaiTest {
 
 	private Partita partita;
 	private ComandoVai vai;
+	private ComandoVai vai2;
+	private ComandoGuarda guarda;
+	private IOConsole io;
 	
 	@BeforeEach
 	void setUp() throws Exception {
+		/**
+		 * IMPORTANTE:
+		 * prima di chiamare un qualsiasi metodo su un oggetto Comando mi devo
+		 * assicurare che sia inizializzato il suo paramentro "IO io" senno
+		 * verrà sollevata una NullPointerException.
+		 */
+		io = new IOConsole();
 		partita = new Partita();
 		vai = new ComandoVai("nord");
-		vai.setIO(new IOConsole());
+		vai.setIO(io);
+		vai2 = new ComandoVai("sud");
+		vai2.setIO(io);
+		guarda = new ComandoGuarda();
+		guarda.setIO(io);
 	}
 
 	// non essendoci il parametro il giocatore non si puo muovere e rimane nella stanza corrente
@@ -51,42 +65,36 @@ class ComandoVaiTest {
 	}
 	
 //	PRIMA DI FARE QUESTI TEST "SCOMMENTARE" GLI IMPORT
-//	@Test 
-//	void testStanzaBloccata(){
-//		Stanza stanza = partita.getStanzaCorrente();
-//		StanzaBloccata bloccata = new StanzaBloccata("b", "ovest", "chiave");
-//		stanza.impostaStanzaAdiacente("sud", bloccata);
-//		bloccata.impostaStanzaAdiacente("nord", stanza);
-//		Stanza stanza2 = new Stanza("Prova");
-//		stanza2.impostaStanzaAdiacente("est", bloccata);
-//		bloccata.impostaStanzaAdiacente("ovest", stanza2);
-//		
-//		ComandoVai vai2 = new ComandoVai("sud");		// comando per spostamento
-//		vai2.esegui(partita);		// mi sposto a sud, quindi nella stanza che ha una direzione bloccata
-//		ComandoGuarda guarda = new ComandoGuarda();
-//		guarda.esegui(partita);
-//		vai2.setParametro("ovest");		// provo ad andare nella direzione bloccata
-//		vai2.esegui(partita);
-//	}
-//	
-//	@Test 
-//	void testStanzaSbloccata(){
-//		Stanza stanza = partita.getStanzaCorrente();
-//		StanzaBloccata bloccata = new StanzaBloccata("b", "ovest", "chiave");
-//		stanza.impostaStanzaAdiacente("sud", bloccata);
-//		bloccata.impostaStanzaAdiacente("nord", stanza);
-//		Stanza stanza2 = new Stanza("Prova");
-//		stanza2.impostaStanzaAdiacente("est", bloccata);
-//		bloccata.impostaStanzaAdiacente("ovest", stanza2);
-//		
-//		ComandoVai vai2 = new ComandoVai("sud");		// comando per spostamento
-//		vai2.esegui(partita);		// mi sposto a sud, quindi nella stanza che ha una direzione bloccata
-//		ComandoGuarda guarda = new ComandoGuarda();
-//		partita.getStanzaCorrente().addAttrezzo(new Attrezzo("chiave", 1));
-//		guarda.esegui(partita);
-//		vai2.setParametro("ovest");		// provo ad andare nella direzione bloccata
-//		vai2.esegui(partita);
-//		
-//	}
+	@Test 
+	void testStanzaBloccata(){
+		Stanza stanza = partita.getStanzaCorrente();		
+		StanzaBloccata bloccata = new StanzaBloccata("b", "ovest", "chiave");
+		stanza.impostaStanzaAdiacente("sud", bloccata);
+		bloccata.impostaStanzaAdiacente("nord", stanza);
+		Stanza stanza2 = new Stanza("Prova");
+		stanza2.impostaStanzaAdiacente("est", bloccata);
+		bloccata.impostaStanzaAdiacente("ovest", stanza2);
+		vai2.esegui(partita);		// mi sposto a sud, quindi nella stanza che ha una direzione bloccata
+		guarda.esegui(partita);
+		vai2.setParametro("ovest");		// provo ad andare nella direzione bloccata
+		vai2.esegui(partita);
+	}
+	
+	@Test 
+	void testStanzaSbloccata(){
+		Stanza stanza = partita.getStanzaCorrente();
+		StanzaBloccata bloccata = new StanzaBloccata("b", "ovest", "chiave");
+		stanza.impostaStanzaAdiacente("sud", bloccata);
+		bloccata.impostaStanzaAdiacente("nord", stanza);
+		Stanza stanza2 = new Stanza("Prova");
+		stanza2.impostaStanzaAdiacente("est", bloccata);
+		bloccata.impostaStanzaAdiacente("ovest", stanza2);
+		vai2.esegui(partita);		// mi sposto a sud, quindi nella stanza che ha una direzione bloccata
+		partita.getStanzaCorrente().addAttrezzo(new Attrezzo("chiave", 1));
+		guarda.esegui(partita);
+		vai2.setParametro("ovest");		// provo ad andare nella direzione bloccata
+		vai2.esegui(partita);
+		
+	}
 
 }
